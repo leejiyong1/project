a2e36d42fe242b5b4653101b0c5efde9664b27ad
leejiyong <rosdlwl005@naver.com>
2024-02-18 23:06:44 +0900

    ddd

--- Ex_Project/src/main/java/com/mega/pro/MemberController.java
+++ Ex_Project/src/main/java/com/mega/pro/MemberController.java
@@ -1,13 +1,19 @@
 package com.mega.pro;
 
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import dao.MemberDAO;
 import util.MyCommon;
@@ -66,21 +72,23 @@ public class MemberController {
 		if (vo == null) {
 			return "no_id";
 		}
-		
+
 		if (!vo.getPwd().equals(pwd)) {
 			return "no_pwd";
-		}else {
-		session = request.getSession();
-		session.setAttribute("mem", vo);
-		return "yes";
+		} else {
+			session = request.getSession();
+			session.setAttribute("mem", vo);
+			return "yes";
 		}
 	}
-	
+
 	@RequestMapping("logout.do")
 	public String logout() {
 		session = request.getSession();
 		session.removeAttribute("mem");
 		return "redirect:login_form.do";
 	}
+
+	
 
 }
